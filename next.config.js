428f87df2a45b8575396d7161ed99df8b9d7fad7
zhurk <rz2606@nyu.edu>
2025-08-25 20:22:32 +0800

    优化代码

--- next.config.js
+++ next.config.js
@@ -10,13 +10,7 @@ const nextConfig = {
     // 在构建时忽略 TypeScript 错误
     ignoreBuildErrors: true,
   },
-<<<<<<< HEAD
-  experimental: {
-    outputFileTracingRoot: undefined,
-  },
-=======
 
->>>>>>> upstream/main
   images: {
     remotePatterns: [
       {
--- prisma/migrations/20250806151913_add_news_pinned_field/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `news` ADD COLUMN `isPinned` BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE `tools` ADD COLUMN `tags` VARCHAR(191) NULL;
